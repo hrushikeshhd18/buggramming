28ef9432cdc18c5f1a32f6d488d884cbf3a736ee
HRUSHIKESH DESAI <48755786+hrushikeshhd18@users.noreply.github.com>
2020-07-30 15:09:16 +0530

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,8 @@
+// particle js config loader
+particlesJS.load(
+  "particle-class",
+  "assets/particlesjs-config.json",
+  function () {
+    console.log("callback - particles.js config loaded");
+  }
+);
